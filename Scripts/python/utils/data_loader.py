4c8da4a616c3cd958c0870fd1c7d761988a80273
Euchie Jn Pierre <113702536+Euchie23@users.noreply.github.com>
2026-03-07 18:28:41 -0400

    Update data_loader.py

--- Scripts/python/utils/data_loader.py
+++ Scripts/python/utils/data_loader.py
@@ -6,8 +6,8 @@ DATA_DIR = Path(__file__).resolve().parent.parent / "data"
 
 @st.cache_data
 def load_pollution_data():
-    return pd.read_csv(DATA_DIR / "squid_pollution.csv", encoding="utf-8")
+    return pd.read_csv(DATA_DIR / "raw"/ "squid_pollution.csv", encoding="utf-8")
 
 @st.cache_data
 def load_modeling_dataset():
-    return pd.read_csv(DATA_DIR / "final_modeling_dataset.csv", encoding="utf-8")
+    return pd.read_csv(DATA_DIR /"processed" / "final_modeling_dataset.csv", encoding="utf-8")
