12a50d719312e4011386a4377dd3ce38b10216f9
Euchie Jn Pierre <113702536+Euchie23@users.noreply.github.com>
2026-06-08 14:23:20 -0400

    Remove dataframe display in interpolation.py
    Removed dataframe display of longitude, latitude, and concentration.

--- Scripts/python/modules/interpolation.py
+++ Scripts/python/modules/interpolation.py
@@ -391,10 +391,6 @@ def render():
     x = agg["longitude_dd"].astype(float).values
     y = agg["latitude_dd"].astype(float).values
     z = agg["concentration"].astype(float).values
-
-    st.dataframe(
-    agg[["longitude_dd","latitude_dd","concentration"]]
-)
     
     # --------------------------------------------------
     # Sparse-data handling
